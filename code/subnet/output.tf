06dac36e6a51116d4cad999430153e64c5844a43
Mahmoud Abo EL Wafaa <82835194+mahmoud2627@users.noreply.github.com>
2024-10-02 04:38:03 +0300

    Update output.tf

--- code/subnet/output.tf
+++ code/subnet/output.tf
@@ -1,1 +1,15 @@
+output "pub_sub1_id" {
+  value = aws_subnet.public["pubSub1"].id
+}
 
+output "pub_sub2_id" {
+  value = aws_subnet.public["pubSub2"].id
+}
+
+output "priv_sub1_id" {
+  value = aws_subnet.private["privSub1"].id
+}
+
+output "priv_sub2_id" {
+  value = aws_subnet.private["privSub2"].id
+}
