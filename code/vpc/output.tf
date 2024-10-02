8d81a6784d8c893a797e44527a06612bd1d7bf85
Mahmoud Abo EL Wafaa <82835194+mahmoud2627@users.noreply.github.com>
2024-10-02 04:39:10 +0300

    Update output.tf

--- code/vpc/output.tf
+++ code/vpc/output.tf
@@ -1,1 +1,12 @@
 
+output "myvpc_id" {
+  value = aws_vpc.vpc1.id
+}
+
+output "pub_sg_id" {
+  value = aws_security_group.pub_sg.id
+}
+
+output "priv_sg_id" {
+  value = aws_security_group.priv_sg.id
+}
