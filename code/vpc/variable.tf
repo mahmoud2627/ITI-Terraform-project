34e863813d4e24498c15cdca9b5456564300ff27
Mahmoud Abo EL Wafaa <82835194+mahmoud2627@users.noreply.github.com>
2024-10-02 04:39:31 +0300

    Update variable.tf

--- code/vpc/variable.tf
+++ code/vpc/variable.tf
@@ -1,1 +1,24 @@
 
+variable "cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "pub_sub1_id" {}
+
+variable "pub_sub2_id" {}
+
+variable "priv_sub1_id" {}
+
+variable "priv_sub2_id" {}
+
+variable "security_rules" {
+  description = "Map of port numbers to CIDR blocks for SSH and HTTP access"
+  type = map(object({
+    port   = number
+    cidr   = string
+  }))
+  default = {
+    ssh  = { port = 22, cidr = "0.0.0.0/0" }
+    http = { port = 80, cidr = "0.0.0.0/0" }
+  }
+}
