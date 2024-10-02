74c00129c9202ad35ed360d82c03afd4db7a9efb
Mahmoud Abo EL Wafaa <82835194+mahmoud2627@users.noreply.github.com>
2024-10-02 04:32:28 +0300

    Update varaible.tf

--- code/Ec2/varaible.tf
+++ code/Ec2/varaible.tf
@@ -1,1 +1,21 @@
+variable "pub_sg_id" {}
 
+variable "priv_sg_id" {}
+
+variable "pub_sub1_id" {}
+
+variable "pub_sub2_id" {}
+
+variable "priv_sub1_id" {}
+
+variable "priv_sub2_id" {}
+
+variable "alb_private_dns" {}
+
+variable "ec2type" {
+  default = "t2.micro"
+}
+
+variable "ec2key" {
+  default = "mah.key"
+}
