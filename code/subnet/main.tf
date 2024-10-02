53f4b619bd38f452862e1ddb990c882a6b1a17e5
Mahmoud Abo EL Wafaa <82835194+mahmoud2627@users.noreply.github.com>
2024-10-02 04:37:43 +0300

    Update main.tf

--- code/subnet/main.tf
+++ code/subnet/main.tf
@@ -1,1 +1,26 @@
+resource "aws_subnet" "public" {
+  for_each = var.public_subnets
 
+  vpc_id                  = var.myvpc_id
+  availability_zone       = each.value.availability_zone
+  cidr_block              = each.value.cidr_block
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = each.key
+  }
+}
+
+
+resource "aws_subnet" "private" {
+  for_each = var.private_subnets
+
+  vpc_id                  = var.myvpc_id
+  availability_zone       = each.value.availability_zone
+  cidr_block              = each.value.cidr_block
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = each.key
+  }
+}
