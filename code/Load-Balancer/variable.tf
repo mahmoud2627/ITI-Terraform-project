5c6084217d96466a4ee339e051e0209bd6907acd
Mahmoud Abo EL Wafaa <82835194+mahmoud2627@users.noreply.github.com>
2024-10-02 04:35:23 +0300

    Update variable.tf

--- code/Load-Balancer/variable.tf
+++ code/Load-Balancer/variable.tf
@@ -1,1 +1,21 @@
+variable "pub_sg_id" {}
 
+variable "priv_sg_id" {}
+
+variable "pub_sub1_id" {}
+
+variable "pub_sub2_id" {}
+
+variable "priv_sub1_id" {}
+
+variable "priv_sub2_id" {}
+
+variable "myvpc_id" {}
+
+variable "nginx1_id" {}
+
+variable "nginx2_id" {}
+
+variable "web1_id" {}
+
+variable "web2_id" {}
