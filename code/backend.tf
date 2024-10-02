00324e43a914b9af1fb2e71a1765b105462e03cd
Mahmoud Abo EL Wafaa <82835194+mahmoud2627@users.noreply.github.com>
2024-10-02 04:40:17 +0300

    Update backend.tf

--- code/backend.tf
+++ code/backend.tf
@@ -1,1 +1,10 @@
 
+
+terraform {
+  backend "s3" {
+    bucket         = "bucket-test-end"
+    key            = "tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "tf-lock-state"
+  }
+} 
