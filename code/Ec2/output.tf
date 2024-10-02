ae289ddb2e960996bc4464cbac70947d26fa27b7
Mahmoud Abo EL Wafaa <82835194+mahmoud2627@users.noreply.github.com>
2024-10-02 04:32:09 +0300

    Update output.tf

--- code/Ec2/output.tf
+++ code/Ec2/output.tf
@@ -1,1 +1,15 @@
+output "nginx1_id" {
+  value = aws_instance.nginx["proxy_1"].id
+}
 
+output "nginx2_id" {
+  value = aws_instance.nginx["proxy_2"].id
+}
+
+output "web1_id" {
+  value = aws_instance.web["web1"].id
+}
+
+output "web2_id" {
+  value = aws_instance.web["web2"].id
+}
